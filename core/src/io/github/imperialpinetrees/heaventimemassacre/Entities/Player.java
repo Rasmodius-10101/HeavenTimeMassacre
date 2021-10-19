8f665ef950673f77d31f0aa2e8ebf1c0a88cdd54
Rasmodius-10101 <91570909+Rasmodius-10101@users.noreply.github.com>
2021-10-19 12:52:28 -0500

    Clean up

--- core/src/io/github/imperialpinetrees/heaventimemassacre/Entities/Player.java
+++ core/src/io/github/imperialpinetrees/heaventimemassacre/Entities/Player.java
@@ -79,19 +79,19 @@ public class Player {
                     playerCoords.x += 1;
                 }
             }
-        }*/
+        }
 
-        /*for (Rectangle e : MapManager.getCollisionArray()) {
+        for (Rectangle e : MapManager.getCollisionArray()) {
             oldPlayerCoords = playerCoords;
 
             if (collisionRectangle.overlaps(e)) {
                 if (collisionRectangle.x >= e.x) {
                     playerCoords.x += 1;
-                } */
-        /*else if (collisionRectangle.x >= e.x+e.width){
+                }
+        else if (collisionRectangle.x >= e.x+e.width){
                     playerCoords.x += 1;
-                }*/
-        /*
+                }
+
                 if (collisionRectangle.y >= e.y) {
                     playerCoords.y += 1;
                 }
@@ -152,65 +152,13 @@ public class Player {
 
     }
 
-    public void getPlayerMovementOld(float deltatime) {
-
-
-
-        if ((Gdx.input.isKeyPressed(Input.Keys.RIGHT)||Gdx.input.isKeyPressed(Input.Keys.D))) {
-
-                if (!isPlayerOverlappingCollisionX()) {
-                    playerCoords.x += velocity.x * deltatime;
-                }
-                else {
-
-                }
-                playerRectangle.x = playerCoords.x;
-                movementDirection = MovementDirection.RIGHT;
-                aimDirection = AimDirection.AIM_RIGHT;
-
-        }
-
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)||Gdx.input.isKeyPressed(Input.Keys.A)) {
-            if (!isPlayerOverlappingCollisionX()) {
-                playerCoords.x -= velocity.x * deltatime;
-            }
-            else {
-                playerCoords.x = playerCoords.x;
-            }
-            playerRectangle.x = playerCoords.x;
-            movementDirection = MovementDirection.LEFT;
-            aimDirection = AimDirection.AIM_LEFT;
-        }
-
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)){
-            if (isPlayerOverlappingCollisionY()) {
-
-            } else {
-                playerCoords.y += velocity.y * deltatime;
-                playerRectangle.y = playerCoords.y;
-                movementDirection = MovementDirection.UP;
-                jumpDistance += playerCoords.y;
-                jumpBlock = jumpDistance >= MAX_JUMP;
-                aimDirection = AimDirection.AIM_UP;
-            }
-
-        }else {
-            if (isPlayerOverlappingCollisionY()) {
-                playerCoords.y = playerCoords.y;
-            } else {
-                playerCoords.y += -getGravity() * deltatime;
-            }
-            playerRectangle.y = playerCoords.y;
-            movementDirection = MovementDirection.DOWN;
-            /*jumpBlock = jumpDistance >= 0;*/
-        }
-    }
 
     //collision
     private static void updateCollisionRectangle(){
         collisionRectangle.setPosition(playerCoords.x, playerCoords.y);
     }
 
+    /*
     private static boolean isPlayerOverlappingCollisionY() {
         for (Rectangle collision : MapManager.getCollisionArray()) {
             if (collisionRectangle.overlaps(collision)) {
@@ -238,6 +186,7 @@ public class Player {
             // if (collisionRectangle.x <= collison.x ||)
         }
     }
+    */
 
     public static void setPosition(float x, float y){
         playerCoords.x = x;
@@ -246,11 +195,12 @@ public class Player {
         updateCollisionRectangle();
     }
 
+
+
     //getters and setters
     public static float getXPos() {
         return playerCoords.x;
     }
-
     public static void setXPos(float x) {
         playerCoords.x = x;
     }
@@ -258,7 +208,6 @@ public class Player {
     public static float getYPos() {
         return playerCoords.y;
     }
-
     public static void setYPos(float y) {
         playerCoords.y = y;
     }
@@ -266,7 +215,6 @@ public class Player {
     public int getWIDTH() {
         return WIDTH;
     }
-
     public void setWIDTH(int WIDTH) {
         this.WIDTH = WIDTH;
     }
@@ -274,7 +222,6 @@ public class Player {
     public int getHEIGHT() {
         return HEIGHT;
     }
-
     public void setHEIGHT(int HEIGHT) {
         this.HEIGHT = HEIGHT;
     }
